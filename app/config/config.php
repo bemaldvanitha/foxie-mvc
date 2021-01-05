e74b50a8ef4e60b55e2ab348b520d7db00af8661
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-01-05 17:29:51 +0530

    auto load core libraries

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+    // app root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL Root
+    define('URLROOT','http://localhost/foxiemvc');
+
+    // site name
+    define('SITENAME','Foxiemvc');
