430525f9d11e16095462fe97f474b37a00373481
bemaldvanitha <bdvidanagamachchi@gmail.com>
2021-01-05 15:07:02 +0530

    load controller by url

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -12,10 +12,34 @@
         protected $params = [];
 
         public function __construct(){
-            $this->getUrl();
+            //print_r($this->getUrl());
+
+            $url = $this->getUrl();
+
+            // look in controllers for first index
+            if(file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
+
+                // if exists set as controller
+                $this->currentController = ucwords($url[0]);
+
+                // unset 0 index
+                unset($url[0]);
+            }
+
+            // require the controller
+            require_once '../app/controllers/'.$this->currentController. '.php';
+
+            // instantiate controller class
+            $this->currentController = new $this->currentController;
         }
 
         public function getUrl(){
-            echo $_GET['url'];
+            if(isset($_GET['url'])){
+                $url = rtrim($_GET['url'],'/');
+                $url = filter_var($url,FILTER_SANITIZE_URL);
+                $url = explode('/',$url);
+                return $url;
+            }
         }
+
     }
